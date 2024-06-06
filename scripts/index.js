fc32068f022f7363375bad60f1733988081d4a22
LegosAndStuff <masonnorberg@midco.net>
2024-06-05 20:54:08 -0500

    made close window function

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,22 @@
-window.onscroll = function() {myFunction()};
+/*
+window.onscroll = function() {scroll()};
 
 var navbar = document.getElementById("navBar");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function scroll() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
     }
 }
+*/
+
+function close() {
+    var win = document.getElementById("win")
+
+    win.classList.add("noWindow")
+}
+
+document.getElementById("close").addEventListener("click", close);
