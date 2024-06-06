f82e2a1dd8a2691ba988f2b9fdc9486ff3e5d3a2
LegosAndStuff <masonnorberg@midco.net>
2024-06-06 07:38:15 -0500

    removed unneeded function

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,3 @@
-window.onscroll = function() {scroll()};
-
-var navbar = document.getElementById("navBar");
-var sticky = navbar.offsetTop;
-
-function scroll() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
-
-
 function close() {
     var win = document.getElementById("win")
     win.classList.add("noWindow")
