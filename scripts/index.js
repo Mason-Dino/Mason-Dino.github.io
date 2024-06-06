0f6a265a00118690f1454a5d2a8db09ebd792095
LegosAndStuff <masonnorberg@midco.net>
2024-06-06 07:36:05 -0500

    made open function

--- scripts/index.js
+++ scripts/index.js
@@ -14,8 +14,29 @@ function scroll() {
 
 function close() {
     var win = document.getElementById("win")
-
     win.classList.add("noWindow")
+
+    var icon = document.getElementById("icon")
+    icon.classList.remove("LaunchIconNo")
+    icon.classList.add("launchIconYes")
+
+    var compScreen = document.getElementById("CompScreen")
+    compScreen.classList.remove("computerScreen")
+    compScreen.classList.add("computerScreenNoWin")
 }
 
+function open() {
+    var win = document.getElementById("win")
+    win.classList.remove("noWindow")
+
+    var icon = document.getElementById("icon")
+    icon.classList.remove("launchIconYes")
+    icon.classList.add("LaunchIconNo")
+
+    var compScreen = document.getElementById("CompScreen")
+    compScreen.classList.remove("computerScreenNoWin")
+    compScreen.classList.add("computerScreen")
+}
+
+document.getElementById("icon").addEventListener("click", open)
 document.getElementById("close").addEventListener("click", close);
