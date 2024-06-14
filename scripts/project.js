29bf84c156e7a54e60a314b96a38a71eb2c4ad7a
LegosAndStuff <masonnorberg@midco.net>
2024-06-14 13:35:10 -0500

    made heart function work

--- scripts/project.js
+++ scripts/project.js
@@ -2,4 +2,17 @@ localStorage.getItem("bob")
 
 function heart(id) {
     console.log(id)
+
+    var heart = document.getElementById(id + "Image").src
+
+    myHeart = heart.split("Portfolio/")
+    heart = myHeart[1]
+
+    if (heart === "style/image/heart-fill.svg") {
+        document.getElementById(id + "Image").src = "style/image/heart-nofill.svg"
+    }
+
+    if (heart === "style/image/heart-nofill.svg") {
+        document.getElementById(id + "Image").src = "style/image/heart-fill.svg"
+    }
 }
