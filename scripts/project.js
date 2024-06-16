2d4f3f9e7d483b6c8f9c99abbdc3863b0f99bced
LegosAndStuff <masonnorberg@midco.net>
2024-06-16 12:02:26 -0500

    made heart stay with local storage

--- scripts/project.js
+++ scripts/project.js
@@ -1,19 +1,54 @@
-localStorage.getItem("bob")
+if (localStorage.getItem("dino") == null) {
+    localStorage.setItem("dino", false)
+    localStorage.setItem("song", false)
+    localStorage.setItem("fun", false)
+    localStorage.setItem("disecon", false)
+}
+
+function getHeart() {
+    for (let i = 0; i < 4; i ++) {
+        projects = ["dino", "song", "fun", "disecon"]
+
+        console.log(localStorage.getItem(projects[i]))
+        
+        if (localStorage.getItem(projects[i]) === "true") {
+            heart(projects[i])
+        }
+    }
+}
 
 function heart(id) {
     console.log(id)
 
-    var heart = document.getElementById(id).src
-    console.log(heart)
-    
     fill = "https://raw.githubusercontent.com/Mason-Dino/Mason-Dino.github.io/29bf84c156e7a54e60a314b96a38a71eb2c4ad7a/style/image/heart-fill.svg"
     nofill = "https://raw.githubusercontent.com/Mason-Dino/Mason-Dino.github.io/29bf84c156e7a54e60a314b96a38a71eb2c4ad7a/style/image/heart-nofill.svg"
+
+    if (id.includes("Image")) {
+        var storage = id.split("Image")
+        storage = storage[0]
+
+        console.log(storage)
+
+        var heart = document.getElementById(id).src
+        console.log(heart)
+
+        if (heart === fill) {
+            document.getElementById(id).src = nofill
+            localStorage.setItem(storage, false)
+        }
     
-    if (heart === fill) {
-        document.getElementById(id).src = nofill
+        if (heart === nofill) {
+            document.getElementById(id).src = fill
+            localStorage.setItem(storage, true)
+        }
     }
 
-    if (heart === nofill) {
-        document.getElementById(id).src = fill
+    else {
+        var heart = document.getElementById(id + "Image").src
+        console.log("hey123") 
+
+        document.getElementById(id + "Image").src = fill
     }
+    
+    
 }
