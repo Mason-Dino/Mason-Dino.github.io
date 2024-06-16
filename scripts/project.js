db870389648c577600bd541229279cc3fd3c989b
LegosAndStuff <masonnorberg@midco.net>
2024-06-16 11:30:10 -0500

    made it only so when you click the hearts fill and not anywhere in the header

--- scripts/project.js
+++ scripts/project.js
@@ -3,17 +3,17 @@ localStorage.getItem("bob")
 function heart(id) {
     console.log(id)
 
-    var heart = document.getElementById(id + "Image").src
+    var heart = document.getElementById(id).src
     console.log(heart)
     
     fill = "https://raw.githubusercontent.com/Mason-Dino/Mason-Dino.github.io/29bf84c156e7a54e60a314b96a38a71eb2c4ad7a/style/image/heart-fill.svg"
     nofill = "https://raw.githubusercontent.com/Mason-Dino/Mason-Dino.github.io/29bf84c156e7a54e60a314b96a38a71eb2c4ad7a/style/image/heart-nofill.svg"
     
     if (heart === fill) {
-        document.getElementById(id + "Image").src = nofill
+        document.getElementById(id).src = nofill
     }
 
     if (heart === nofill) {
-        document.getElementById(id + "Image").src = fill
+        document.getElementById(id).src = fill
     }
 }
