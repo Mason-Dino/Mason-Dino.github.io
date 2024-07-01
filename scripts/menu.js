0f9e6b91e735e8f662e1a845f464d2c92dbd3d88
LegosAndStuff <masonnorberg@midco.net>
2024-07-01 12:34:25 -0500

    add close icon

--- scripts/menu.js
+++ scripts/menu.js
@@ -11,6 +11,9 @@ function menu() {
         menu.classList.remove("noMenu")
         menu.classList.add("yesMenu")
 
+        var menuButton = document.getElementById("menuButton")
+        menuButton.src = "style/image/close.svg"
+
         menuActive = true
     }
 
@@ -21,6 +24,9 @@ function menu() {
         var menu = document.getElementById("menu")
         menu.classList.add("noMenu")
         menu.classList.remove("yesMenu")
+
+        var menuButton = document.getElementById("menuButton")
+        menuButton.src = "style/image/menu.svg"
 
         menuActive = false
     }
